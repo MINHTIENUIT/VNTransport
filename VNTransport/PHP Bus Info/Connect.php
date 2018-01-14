bccfbc5988c358cf0d00448330f9297370f7c8a5
huuphuoc1396 <huuphuoc1396@gmail.com>
2018-01-15 01:10:38 +0700

    Clean up for all project to convert to Laravel framework.

--- VNTransport/PHP Bus Info/Connect.php
+++
@@ -1,4 +0,0 @@
-<?php 
-	$con = mysqli_connect('localhost','root','','vntransport2');
-	mysqli_set_charset($con , "utf8");
-?>
--- VNTransport/PHP Bus Info/delcmt.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include 'Connect.php';
-$delcmt = $_REQUEST['del'];
-mysqli_query($con,"DELETE FROM cmt WHERE ID='$delcmt'");
-echo $delcmt;
-//echo "<script>window.location='BusInformation.php'</script>"; 
-?>
